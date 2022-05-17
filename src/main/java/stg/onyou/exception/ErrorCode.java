5ccde728d68e08e875f071c093cc97cc08bd5f05
Junyong Jang <aszx6992@naver.com>
2022-05-17 14:21:21 +0900

    ErrorCode 커밋 반영

--- src/main/java/stg/onyou/exception/ErrorCode.java
+++ src/main/java/stg/onyou/exception/ErrorCode.java
@@ -10,14 +10,15 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    /*
+
     // 401 UNAUTHORIZED : 인증되지 않은 사용자
-    INVALID_AUTH_TOKEN(UNAUTHORIZED, "권한 정보가 없는 토큰입니다"),
-    UNAUTHORIZED_MEMBER(UNAUTHORIZED, "현재 내 계정 정보가 존재하지 않습니다"),
-    */
+    //INVALID_AUTH_TOKEN(UNAUTHORIZED, "권한 정보가 없는 토큰입니다"),
+    //UNAUTHORIZED_MEMBER(UNAUTHORIZED, "현재 내 계정 정보가 존재하지 않습니다"),
+
     // 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재
     DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),
     CLUB_MEMBER_FULL(CONFLICT, "클럽의 정원이 마감되었습니다"),
+    DUPLICATE_CATEGORY(CONFLICT, "동일한 카테고리를 2개 선택하였습니다"),
 
 
     // 404 NOT_FOUND : Resource 를 찾을 수 없음
